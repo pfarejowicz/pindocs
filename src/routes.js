774dc43a4b2f2259190056f9e4e9a1c20bc525b7
Peter Farejowicz <pfarejowicz@pinterest.com>
2018-04-04 15:11:58 -0700

    Fix regex matcher

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const requireCard = require.context('./pages', true, /\.js$/);
 const paths = requireCard.keys();
 paths.sort((a, b) => a.localeCompare(b));
 paths.forEach(path => {
-  const pathname = path.match(/\.\/(.+)\.js$/)[1];
+  const pathname = path.match(/([^/]+)\.js$/)[1];
   routes[pathname] = requireCard(path).default;
 });
 
